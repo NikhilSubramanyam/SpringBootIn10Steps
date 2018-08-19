a718e498a53d85909caf90b8659d19948b3a3fe7
Nikhil Subramanyam <nikhilsubramanyam@Nikhils-MacBook-Pro.local>
2018-08-18 20:06:26 -0400

    Testing

--- src/main/java/com/nikhil/springboot/basic/SpringBootIn10Steps/Books.java
+++ src/main/java/com/nikhil/springboot/basic/SpringBootIn10Steps/Books.java
@@ -1,5 +1,28 @@
 package com.nikhil.springboot.basic.SpringBootIn10Steps;
 
 public class Books {
+	
+	long id;
+	String title;
+	String author;
+	
+	public long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Books(long id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
 
 }
